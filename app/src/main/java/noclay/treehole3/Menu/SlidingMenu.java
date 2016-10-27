6ea8ba3b04eb8ea51895224865684bf4cf788a70
NoClay <826611389@qq.com>
2016-10-27 22:13:09 +0800

    修改了页面UI，有些按钮并未实现功能

---
+++ app/src/main/java/noclay/treehole3/Menu/SlidingMenu.java
@@ -0,0 +1,284 @@
+package noclay.treehole3.Menu;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+
+import com.nineoldandroids.view.ViewHelper;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import noclay.treehole3.R;
+
+public class SlidingMenu extends HorizontalScrollView {
+    private LinearLayout mWapper;//包含菜单视图和内容视图的HorizontalScrollView
+    private ViewGroup mMenu;//菜单视图
+    private ViewGroup mContent;//内容的视图
+    private int mScreenWidth;//屏幕的宽度
+    private int mMenuWidth;//菜单的宽度
+    private int mMenuRightPadding = 50;//菜单弹出后距离右侧的距离，单位为dp
+    private boolean once;//将初始化大小的OnMeasure()中的初始化菜单和内容的大小的代码段设置为执行一次
+    private boolean isOpen;//描述菜单的打开状态
+
+
+    private int lastX;
+    private int lastY;
+    private boolean lastState;
+
+    private static final int MSG_FOR_OPEN_CHANGED = 200;
+    private static final String TAG = "SlidingMenu";
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    /**
+     * 未使用自定义属性时，调用
+     *
+     * @param context
+     * @param attrs
+     */
+    public SlidingMenu(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    /**
+     * 当使用了自定义属性时，会调用此构造方法
+     *
+     * @param context
+     * @param attrs
+     * @param defStyle
+     */
+    public SlidingMenu(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        // 获取我们定义的属性
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.SlidingMenu, defStyle, 0);
+
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.SlidingMenu_rightPadding://自定义弹出菜单右侧的宽度
+                    mMenuRightPadding = a.getDimensionPixelSize(attr,
+                            (int) TypedValue.applyDimension(//单位转换
+                                    TypedValue.COMPLEX_UNIT_DIP, 50, context
+                                            .getResources().getDisplayMetrics()));
+                    break;
+            }
+        }
+        a.recycle();
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        mScreenWidth = outMetrics.widthPixels;//获取屏幕的宽度
+
+    }
+
+    public SlidingMenu(Context context) {
+        this(context, null);
+    }
+
+    /**
+     * 设置子View的宽和高 设置自己的宽和高
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (!once) {
+            mWapper = (LinearLayout) getChildAt(0);//获取HorizontalScrollView里边子布局LinearLayout
+            mMenu = (ViewGroup) mWapper.getChildAt(0);//获取LinearLayout里边的第一个子布局即菜单布局
+            mContent = (ViewGroup) mWapper.getChildAt(1);//获取第二个布局，即内容布局
+            mMenuWidth = mMenu.getLayoutParams().width = mScreenWidth
+                    - mMenuRightPadding;//设置菜单视图的宽度
+            mContent.getLayoutParams().width = mScreenWidth;//设置内容视图的宽度
+            once = true;
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    /**
+     * 通过设置偏移量，将menu隐藏
+     */
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b)//决定布局的位置
+    {
+        super.onLayout(changed, l, t, r, b);
+        if (changed) {
+            this.scrollTo(mMenuWidth, 0);
+        }
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                lastX = (int) ev.getX();
+                lastY = (int) ev.getY();
+
+                break;
+            }
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_UP:
+                // 隐藏在左边的宽度
+//                int scrollX = getScrollX();
+//                Log.d(TAG, "onTouchEvent: scrollX" + scrollX);
+//                Log.d(TAG, "onTouchEvent: menuWidth" + mMenuWidth);
+//                Log.d(TAG, "onTouchEvent: touch" + mMenuWidth / 16);
+
+//                if (scrollX >= mMenuWidth / 2)//大于菜单的一半，隐藏
+//                {
+//                    this.smoothScrollTo(mMenuWidth, 0);
+//                    isOpen = false;
+//                } else
+//                {
+//                    this.smoothScrollTo(0, 0);
+//                    isOpen = true;
+//                }
+
+                if (Math.abs(ev.getX() - lastX) >= Math.abs(ev.getY() - lastY)) {
+                    if (!isOpen && (ev.getX() - lastX) >= 100) {
+                        //向右滑动操作
+                        openMenu();
+
+                    }
+                    if (isOpen && (lastX - ev.getX()) >= 100) {
+                        closeMenu();
+                    }
+                }
+                return true;
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    /**
+     * 打开菜单
+     */
+    public void openMenu() {
+        if (isOpen)
+            return;
+        this.smoothScrollTo(0, 0);
+        isOpen = true;
+        mOnOpenStateChangeListener.isOpening(true);
+    }
+
+    public void closeMenu() {
+        if (!isOpen)
+            return;
+        this.smoothScrollTo(mMenuWidth, 0);
+        isOpen = false;
+        mOnOpenStateChangeListener.isOpening(false);
+    }
+
+    /**
+     * 切换菜单
+     */
+    public void toggle() {
+        if (isOpen) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    }
+
+    /**
+     * 滚动发生时
+     */
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt)//设置滚动的效果
+    {
+        super.onScrollChanged(l, t, oldl, oldt);
+        float scale = l * 1.0f / mMenuWidth; // 1 ~ 0
+
+        /**
+         * 区别1：内容区域1.0~0.7 缩放的效果 scale : 1.0~0.0 0.7 + 0.3 * scale
+         *
+         * 区别2：菜单的偏移量需要修改
+         *
+         * 区别3：菜单的显示时有缩放以及透明度变化 缩放：0.7 ~1.0 1.0 - scale * 0.3 透明度 0.6 ~ 1.0
+         * 0.6+ 0.4 * (1- scale) ;
+         *
+         */
+        float rightScale = 0.7f + 0.3f * scale;
+        float leftScale = 1.0f - 0.3f * scale;
+        float leftAlpha = 0.6f + 0.4f * (1 - scale);
+
+        // 调用属性动画，设置TranslationX
+        ViewHelper.setTranslationX(mMenu, mMenuWidth * scale * 0.8f);
+
+        ViewHelper.setScaleX(mMenu, leftScale);
+//        ViewHelper.setScaleY(mMenu, leftScale);
+//        ViewHelper.setAlpha(mMenu, leftAlpha);
+        // 设置content的缩放的中心点
+//        ViewHelper.setPivotX(mContent, 0);
+//        ViewHelper.setPivotY(mContent, mContent.getHeight() / 2);
+//        ViewHelper.setScaleX(mContent, rightScale);
+//        ViewHelper.setScaleY(mContent, rightScale);
+
+    }
+
+    /**
+     * 尝试自定义监听事件
+     */
+    public interface onOpenStateChangeListener {
+        void isOpening(boolean flag);
+    }
+
+    onOpenStateChangeListener mOnOpenStateChangeListener = null;
+
+    public void setOnOpenStateChangeListener(onOpenStateChangeListener e) {
+//        this.run();
+        mOnOpenStateChangeListener = e;
+    }
+
+//    Timer timer = new Timer();
+//    TimerTask timerTask = new TimerTask() {
+//        @Override
+//        public void run() {
+//            Log.d(TAG, "run: run");
+//            Message message = new Message();
+//            message.what = MSG_FOR_OPEN_CHANGED;
+//            message.arg1 = isOpen ? 1 : 0;
+//            handler.sendMessage(message);
+//        }
+//    };
+//
+//    public void run(){
+//        timer.schedule(timerTask, 500, 500);//每五秒执行一次handler
+//        Log.d(TAG, "run: timer");
+//    }
+
+//    Handler handler = new Handler() {
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+//            switch (msg.what) {
+//                case MSG_FOR_OPEN_CHANGED: {
+//                    mOnOpenStateChangeListener.isOpening(msg.arg1);
+//                    break;
+//                }
+//            }
+//        }
+//    };
+
+}
